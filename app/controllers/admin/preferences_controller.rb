a33fc06471da3fea243d3b9a73f769bfce43b58d
Samuel Cornet <samuel@noukod.com>
2019-01-30 22:53:50 +0000

    Automatically backed up by Learn

--- app/controllers/admin/preferences_controller.rb
+++ app/controllers/admin/preferences_controller.rb
@@ -49,4 +49,4 @@ class Admin::PreferencesController < ApplicationController
 #   def preference_params
 #     params.require(:preference).permit(:song_sort_order, :artist_sort_order, :allow_create_songs, :allow_create_artists)
 #   end
-# end
+end
